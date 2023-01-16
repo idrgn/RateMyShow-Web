eb231da30a11d47b81778aef5e0eb13caa877e69
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-01-16 10:06:44 +0100

    Modificada URL MOCK API

--- ratemyshow/src/Environment.js
+++ ratemyshow/src/Environment.js
@@ -1,3 +1,3 @@
-const BASE_REAL_URL = "http://localhost:3000"
-const BASE_MOCK_URL = "https://63bbd569cf99234bfa657793.mockapi.io/"
+const BASE_REAL_URL = "http://localhost:3000";
+const BASE_MOCK_URL = "https://0ee0ee41-ff72-4ce8-a306-41b3a57f8eb0.mock.pstmn.io";
 const BASE_URL = BASE_MOCK_URL;
