57aceaff4a33b2c6119cc15de6e7247b34e21c4d
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-01-18 10:22:59 +0100

    Añadida ruta para ContainerFooter

--- ratemyshow/src/App.js
+++ ratemyshow/src/App.js
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ContainerHeader from "./screens/header/ContainerHeader";
 import ContainerMenu from "./screens/menu/ContainerMenu";
+import ContainerFooter from "./screens/footer/ContainerFooter";
 import NotFound from "./screens/notFound/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<ContainerHeader />}>
-        <Route path="/" element={<ContainerMenu />}></Route>
+        <Route path="/" element={<ContainerFooter />}>
+          <Route path="/" element={<ContainerMenu />}></Route>
+        </Route>
       </Route>
 
       <Route path="*" element={<NotFound />}></Route>
