400748886a4a7d6340876f826f1b61ae92a9a3c0
Miguel <mdiazh@fpcoruna.afundacion.org>
2023-01-18 09:26:50 +0100

    Añadida ruta a página principal

--- ratemyshow/src/App.js
+++ ratemyshow/src/App.js
@@ -1,19 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import ContainerHeader from "./screens/header/ContainerHeader";
+import MainPage from "./screens/main_page/MainPage";
 import ContainerMenu from "./screens/menu/ContainerMenu";
 import NotFound from "./screens/notFound/NotFound";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<ContainerHeader />}>
-        <Route path="/" element={<ContainerMenu />}></Route>
-      </Route>
-
-      <Route path="*" element={<NotFound />}></Route>
-    </Routes>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<ContainerHeader />}>
+				<Route path="" element={<MainPage />}></Route>
+				<Route path="/" element={<ContainerMenu />}></Route>
+			</Route>
+			<Route path="*" element={<NotFound />}></Route>
+		</Routes>
+	);
 }
 
 export default App;
