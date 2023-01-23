0a1bd4127c08a66b5b65afeae87d69495fa2d2f5
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-01-23 12:05:45 +0100

    Añadida ruta de UserProfile a App.js

--- ratemyshow/src/App.js
+++ ratemyshow/src/App.js
@@ -8,23 +8,25 @@ import MainPage from "./screens/main_page/MainPage";
 import ContainerMenu from "./screens/menu/ContainerMenu";
 import NotFound from "./screens/notFound/NotFound";
 import Recommendations from "./screens/recommendations/Recommendations";
+import UserProfile from "./screens/userProfile/UserProfile";
 
 function App() {
-	return (
-		<Routes>
-			<Route path="/" element={<ContainerHeader />}>
-				<Route path="/" element={<ContainerFooter />}>
-					<Route path="" element={<MainPage />}></Route>
-					<Route path="/" element={<ContainerMenu />}>
-						<Route path="users/:username/followers" element={<FollowerList />}></Route>
-						<Route path="latest" element={<Latest />}></Route>
-						<Route path="/recommendations" element={<Recommendations />}></Route>
-					</Route>
-				</Route>
-			</Route>
-			<Route path="*" element={<NotFound />}></Route>
-		</Routes>
-	);
+  return (
+    <Routes>
+      <Route path="/" element={<ContainerHeader />}>
+        <Route path="/" element={<ContainerFooter />}>
+          <Route path="" element={<MainPage />}></Route>
+          <Route path="/" element={<ContainerMenu />}>
+            <Route path="users/:username" element={<UserProfile />}></Route>
+            <Route path="users/:username/followers" element={<FollowerList />}></Route>
+            <Route path="latest" element={<Latest />}></Route>
+            <Route path="/recommendations" element={<Recommendations />}></Route>
+          </Route>
+        </Route>
+      </Route>
+      <Route path="*" element={<NotFound />}></Route>
+    </Routes>
+  );
 }
 
 export default App;
