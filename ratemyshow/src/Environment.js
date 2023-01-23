774f8552d3d08f6334fdf6e6ce123fe5d3d2a11c
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-01-23 23:49:58 +0100

    Modificada BASE_MOCK_URL

--- ratemyshow/src/Environment.js
+++ ratemyshow/src/Environment.js
@@ -1,3 +1,3 @@
 const BASE_REAL_URL = "http://localhost:3000";
-const BASE_MOCK_URL = "https://0ee0ee41-ff72-4ce8-a306-41b3a57f8eb0.mock.pstmn.io";
+const BASE_MOCK_URL = "http://api.ratemyshow.lekiam.net/";
 const BASE_URL = BASE_MOCK_URL;
