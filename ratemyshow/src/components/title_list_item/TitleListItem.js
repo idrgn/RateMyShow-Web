b05d364628bf336fb8b7ade086a7adcdf7d40a2e
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-01-25 10:55:46 +0100

    Actualizado código para visualizar cover

--- ratemyshow/src/components/title_list_item/TitleListItem.js
+++ ratemyshow/src/components/title_list_item/TitleListItem.js
@@ -6,9 +6,9 @@
 const TitleListItem = (props) => {
 	return (
 		<div>
+			<img src={props.title.cover}></img>
 			<h3>{props.title.title}</h3>
 			<p>{props.title.year}</p>
-			<p>{props.title.dateAdded}</p>
 		</div>
 	);
 };
