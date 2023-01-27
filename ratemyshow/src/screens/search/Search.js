f9e846165a4a58e076904442dc550c73e935d14f
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-27 13:41:09 +0100

    Código básico de pantalla Search

--- ratemyshow/src/screens/search/Search.js
+++ ratemyshow/src/screens/search/Search.js
@@ -1,5 +1,40 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import TitleListItem from "../../components/title_list_item/TitleListItem";
+
 const Search = () => {
-	return <div></div>;
+	// Parámetros de URL
+	const [searchParams, setSearchParams] = useSearchParams();
+
+	// Se almacenan los parámetros
+	const search = searchParams.get("query");
+	let page = searchParams.get("page");
+
+	// Si la página no está definida, es 0
+	if (page === undefined) page = 0;
+
+	// Creamos estado para almacenar la respuesta
+	const [searchResults, setSearchResults] = useState({ result: [] });
+
+	// Pedimos los datos a la API
+	useEffect(() => {
+		axios.get(`http://api.ratemyshow.lekiam.net/titles?query=${search}&$page={page}`).then((response) => {
+			console.log(`http://api.ratemyshow.lekiam.net/titles?query=${search}`);
+			setSearchResults(response.data);
+		});
+	}, [search, searchParams.query]);
+
+	// Función para transformar títulos a componente.
+	const titleToComponent = (t) => {
+		return <TitleListItem title={t} />;
+	};
+
+	return (
+		<div>
+			<div>{searchResults.result.map(titleToComponent)}</div>
+		</div>
+	);
 };
 
 export default Search;
