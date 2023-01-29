d0e9d2c01f93a60bdcb1155fd06fcc1eeee07e81
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-29 18:52:00 +0100

    Ajustados estilos de Login y uso de inputs MUI

--- ratemyshow/src/screens/login/Login.js
+++ ratemyshow/src/screens/login/Login.js
@@ -1,3 +1,4 @@
+import { TextField, Typography } from "@mui/material";
 import Alert from "@mui/material/Alert";
 import axios from "axios";
 import { useRef, useState } from "react";
@@ -98,17 +99,16 @@ const Login = () => {
 	return (
 		<div className="login-container">
 			<div className="login-title">
-				<h1>Inicio de sesión</h1>
+				<Typography variant="h3">Inicio de sesión</Typography>
 			</div>
 			<div className="login-form-container">
 				<form onSubmit={handleLogin} className="login-form">
 					<div className="login-input-container">
-						<div className="login-required login-input-text">Identificador</div>
-						<input name="username" type="text" ref={identifierRef} maxLength={32}></input>
+						<TextField required id="identifier-input" label="Identificador" type="text " autoComplete="current-username" inputRef={identifierRef} className="login-text-field" />
 					</div>
+
 					<div className="login-input-container">
-						<div className="login-required login-input-text">Contraseña</div>
-						<input name="password" type="password" ref={passwordRef} maxLength={32}></input>
+						<TextField required id="password-input" label="Contraseña" type="password" autoComplete="current-password" inputRef={passwordRef} className="login-text-field" />
 					</div>
 
 					<div>{warning}</div>
