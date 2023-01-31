40fff30604f2e31a9ea940a49d812dd6427cbd07
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-31 12:03:31 +0100

    Añadido indicador de carga en Favorito y Pendiente

--- ratemyshow/src/components/title_list_item/TitleListItem.js
+++ ratemyshow/src/components/title_list_item/TitleListItem.js
@@ -5,6 +5,7 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
+import CircularProgress from "@mui/material/CircularProgress";
 import Typography from "@mui/material/Typography";
 import axios from "axios";
 import { useState } from "react";
@@ -18,27 +19,46 @@ const TitleListItem = (props) => {
 	const [isFavorite, setIsFavorite] = useState(props.title.isFavorite);
 	const [isPending, setIsPending] = useState(props.title.isPending);
 
+	const [isFavoriteLoading, setIsFavoriteLoading] = useState(false);
+	const [isPendingLoading, setIsPendingLoading] = useState(false);
+
 	const handleFavorite = () => {
+		setIsFavoriteLoading(true);
+
 		if (isFavorite) {
-			axios.delete(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/favorite`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-				setIsFavorite(false);
-			});
+			axios
+				.delete(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/favorite`, { headers: { SessionToken: localStorage.getItem("sessionToken") } })
+				.then((response) => {
+					setIsFavorite(false);
+				})
+				.finally(setIsFavoriteLoading(false));
 		} else {
-			axios.put(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/favorite`, {}, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-				setIsFavorite(true);
-			});
+			axios
+				.put(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/favorite`, {}, { headers: { SessionToken: localStorage.getItem("sessionToken") } })
+				.then((response) => {
+					setIsFavorite(true);
+				})
+				.finally(setIsFavoriteLoading(false));
 		}
 	};
 
 	const handlePending = () => {
+		setIsPendingLoading(true);
+
 		if (isPending) {
-			axios.delete(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/pending`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-				setIsPending(false);
-			});
+			axios
+				.delete(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/pending`, { headers: { SessionToken: localStorage.getItem("sessionToken") } })
+				.then((response) => {
+					setIsPending(false);
+				})
+				.finally(setIsPendingLoading(false));
 		} else {
-			axios.put(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/pending`, {}, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-				setIsPending(true);
-			});
+			axios
+				.put(`http://api.ratemyshow.lekiam.net/titles/${props.title.id}/pending`, {}, { headers: { SessionToken: localStorage.getItem("sessionToken") } })
+				.then((response) => {
+					setIsPending(true);
+				})
+				.finally(setIsPendingLoading(false));
 		}
 	};
 
@@ -55,10 +75,10 @@ const TitleListItem = (props) => {
 			</CardContent>
 			<CardActions disableSpacing>
 				<IconButton aria-label="add to favorites" onClick={handleFavorite}>
-					<FavoriteIcon htmlColor={isFavorite ? "red" : "grey"} />
+					{isFavoriteLoading ? <CircularProgress size={20} /> : <FavoriteIcon htmlColor={isFavorite ? "red" : "grey"} />}
 				</IconButton>
 				<IconButton aria-label="add to pending" onClick={handlePending}>
-					<AddToQueueIcon htmlColor={isPending ? "blue" : "grey"} />
+					{isPendingLoading ? <CircularProgress size={20} /> : <AddToQueueIcon htmlColor={isPending ? "blue" : "grey"} />}
 				</IconButton>
 			</CardActions>
 		</Card>
