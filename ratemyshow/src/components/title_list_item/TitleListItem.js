5d0f945204f5f23ea66f5b694691c960216231f5
idrgn <37042855+idrgn@users.noreply.github.com>
2023-01-31 10:33:52 +0100

    Implementados componentes de MUI en TitleListItem

--- ratemyshow/src/components/title_list_item/TitleListItem.js
+++ ratemyshow/src/components/title_list_item/TitleListItem.js
@@ -1,3 +1,12 @@
+import IconButton from "@material-ui/core/IconButton";
+import AddToQueueIcon from "@material-ui/icons/AddToQueue";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
 /**
  * Representa un título de una lista de títulos
  * @param {*} props
@@ -5,11 +14,25 @@
  */
 const TitleListItem = (props) => {
 	return (
-		<div>
-			<img src={props.title.cover}></img>
-			<h3>{props.title.title}</h3>
-			<p>{props.title.year}</p>
-		</div>
+		<Card sx={{ maxWidth: 200, maxHeight: 600 }} variant="outlined">
+			<CardMedia sx={{ height: 300, width: 200 }} image={props.title.cover} title="Title" />
+			<CardContent>
+				<Typography noWrap gutterBottom variant="h6">
+					{props.title.primaryTitle}
+				</Typography>
+				<Typography noWrap gutterBottom variant="subtitle1">
+					{props.title.startYear}
+				</Typography>
+			</CardContent>
+			<CardActions disableSpacing>
+				<IconButton aria-label="add to favorites">
+					<FavoriteIcon />
+				</IconButton>
+				<IconButton aria-label="add to pending">
+					<AddToQueueIcon />
+				</IconButton>
+			</CardActions>
+		</Card>
 	);
 };
 
