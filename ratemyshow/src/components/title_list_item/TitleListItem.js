f60065855eee75b7164fcf80a9a0ae873e88ff3a
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-01 22:52:04 +0100

    Modificado estilo de TitleListItem

--- ratemyshow/src/components/title_list_item/TitleListItem.js
+++ ratemyshow/src/components/title_list_item/TitleListItem.js
@@ -1,7 +1,6 @@
-import { Star } from "@mui/icons-material";
 import AddToQueueIcon from "@mui/icons-material/AddToQueue";
 import FavoriteIcon from "@mui/icons-material/Favorite";
-import { Divider } from "@mui/material";
+import { Divider, Tooltip } from "@mui/material";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -9,6 +8,7 @@ import CardMedia from "@mui/material/CardMedia";
 import CircularProgress from "@mui/material/CircularProgress";
 import IconButton from "@mui/material/IconButton";
 import Typography from "@mui/material/Typography";
+import { Box } from "@mui/system";
 import axios from "axios";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
@@ -75,18 +75,32 @@ const TitleListItem = (props) => {
 		}
 	};
 
+	const capitalizeFirstLetter = (string) => {
+		return string.charAt(0).toUpperCase() + string.slice(1);
+	};
+
 	return (
 		<Card sx={{ maxWidth: 200, maxHeight: 600 }} variant="outlined">
-			<CardMedia sx={{ height: 300, width: 200, cursor: "pointer" }} image={props.title.cover ? props.title.cover : "http://api.ratemyshow.lekiam.net/img/cover"} title="Title" onClick={hanldeRedirect} />
-			<CardContent>
-				<Typography className="titlelistitem-title" align="center" gutterBottom variant="h6" sx={{ height: 66, overflow: "scroll", msOverflowStyle: "none", scrollbarWidth: "none", WebkitLineClamp: 2, WebkitAlignContent: "center", display: "-webkit-box", WebkitBoxOrient: "vertical" }}>
-					{props.title.primaryTitle}
-				</Typography>
+			<CardMedia className="titlelistitem-card-media" sx={{ height: 300, width: 200, cursor: "pointer" }} image={props.title.cover ? props.title.cover : "http://api.ratemyshow.lekiam.net/img/cover"} title="Title" onClick={hanldeRedirect} />
+			<Divider></Divider>
 
-				<Divider sx={{ marginTop: 2, marginBottom: 1 }}></Divider>
-				<Typography noWrap gutterBottom variant="subtitle1">
-					{props.title.startYear} {props.title.endYear ? "-" : ""} {props.title.endYear ? props.title.endYear : ""}
-				</Typography>
+			<CardContent sx={{ pb: 0, pl: 0, pr: 0 }}>
+				<Tooltip title={props.title.primaryTitle} placement="top">
+					<Typography className="titlelistitem-title" align="center" gutterBottom variant="h6" sx={{ fontWeight: "bold", height: 66, overflow: "scroll", WebkitLineClamp: 2, WebkitAlignContent: "center", display: "-webkit-box", WebkitBoxOrient: "vertical" }}>
+						{props.title.primaryTitle}
+					</Typography>
+				</Tooltip>
+
+				<Divider sx={{ mb: 2, mt: 2 }}></Divider>
+
+				<Box sx={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+					<Typography noWrap gutterBottom variant="subtitle1">
+						{props.title.startYear} {props.title.endYear ? "-" : ""} {props.title.endYear ? props.title.endYear : ""}
+					</Typography>
+					<Typography noWrap gutterBottom variant="subtitle2">
+						{capitalizeFirstLetter(props.title.titleType)}
+					</Typography>
+				</Box>
 			</CardContent>
 			<CardActions disableSpacing>
 				<IconButton aria-label="add to favorites" onClick={handleFavorite} disabled={isFavorite === undefined}>
@@ -95,7 +109,7 @@ const TitleListItem = (props) => {
 				<IconButton aria-label="add to pending" onClick={handlePending} disabled={isFavorite === undefined}>
 					{isPendingLoading ? <CircularProgress size={20} /> : <AddToQueueIcon htmlColor={isPending ? "blue" : "grey"} />}
 				</IconButton>
-				<Typography sx={{ marginLeft: "auto", marginRight: "0.5vw", color: "grey" }}>{props.title.rating ? props.title.rating.toFixed(1) : "No ratings"}</Typography>
+				<Typography sx={{ marginLeft: "auto", marginRight: "0.5vw", color: "grey" }}>{props.title.rating ? props.title.rating.toFixed(1) + " / 5.0" : "No ratings"}</Typography>
 			</CardActions>
 		</Card>
 	);
