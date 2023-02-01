f8ca7b35101b53a6c23b1bc74c6ad5d9210df699
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-01 21:19:53 +0100

    Ajustado CSS de search y añadida carga

--- ratemyshow/src/screens/search/Search.js
+++ ratemyshow/src/screens/search/Search.js
@@ -2,55 +2,38 @@ import { Pagination } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import Loading from "../../components/loading/Loading";
 import TitleList from "../../components/title_list/TitleList";
 import "./Search.css";
 
 const Search = () => {
-	// Parámetros de URL
-	const [searchParams, setSearchParams] = useSearchParams();
-
-	// Página actual
 	const [page, setPage] = useState(0);
-
-	// Resultados almacenados por página
-	const [stored, setStored] = useState({});
-
-	// Se almacenan los parámetros
-	const search = searchParams.get("query");
-
-	// Creamos estado para almacenar la respuesta
+	const [isLoading, setIsLoading] = useState(true);
+	const [searchParams, setSearchParams] = useSearchParams();
 	const [searchResults, setSearchResults] = useState({ result: [] });
+
+	// Se obtiene la query
+	const search = searchParams.get("query");
 
 	// Obtención de datos
 	useEffect(() => {
-		if (page in stored) {
-			// Si los datos ya están almacenados, se cargan
-			setSearchResults(stored[page]);
-		} else {
-			// Si no, pedimos los datos a la API
-			axios.get(`http://api.ratemyshow.lekiam.net/titles?query=${search}&page=${page}`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-				setSearchResults(response.data);
-				const newStored = stored;
-				newStored[page] = response.data;
-				setStored(newStored);
-			});
-		}
-	}, [page]);
+		setIsLoading(true);
+		axios.get(`http://api.ratemyshow.lekiam.net/titles?query=${search}&page=${page}`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
+			setSearchResults(response.data);
+			setPage(response.data.current);
+		});
+	}, [page, search]);
 
-	// Actualizar página
 	const onPageChange = (event, value) => {
-		// Solo se actualiza si el valor cambia
 		if (page !== value - 1) {
 			setPage(value - 1);
 		}
 	};
 
 	return (
-		<div>
+		<div className="search-container">
 			<div className="search-title">Resultados de la búsqueda "{search}"</div>
-			<div className="search-result">
-				<TitleList titles={searchResults.result}></TitleList>
-			</div>
+			<div className="search-result">{isLoading ? <Loading /> : <TitleList titles={searchResults.result}></TitleList>}</div>
 			<div className="search-pagination" color="primary" size="large">
 				<Pagination count={searchResults.pages} onChange={onPageChange} />
 			</div>
