5bfdb80e8665f98dffd69a622cdc6ea2458cd0c4
slusquinost22 <slusquinost22@fpcoruna.afundacion.org>
2023-02-01 10:01:00 +0100

    Refactorización de Recommendations

--- ratemyshow/src/screens/recommendations/Recommendations.js
+++ ratemyshow/src/screens/recommendations/Recommendations.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import RecommendationList from "../../components/recommendation_list/RecommendationList";
+import TitleList from "../../components/title_list/TitleList";
 import "./Recommendations.css";
 
 /**
@@ -14,20 +14,26 @@ const Recommendations = (props) => {
 
 	// Pedimos los datos a la API
 	useEffect(() => {
-		axios.get(`https://api.ratemyshow.lekiam.net/recommendations`).then((response) => {
+		axios.get(`http://api.ratemyshow.lekiam.net/recommendations`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
 			setRecommendations(response.data);
+			console.log(JSON.stringify(response.data));
 		});
 	}, []);
 
 	// Función para convertir seguidor a componente
 	const recommendationsToComponent = (r) => {
-		return <RecommendationList recommendations={r} />;
+		return (
+			<div>
+				<div className="recommendations-genre">{r.genre}</div>
+				<TitleList titles={r.titles}></TitleList>
+			</div>
+		);
 	};
 
 	return (
 		<div className="recommendations">
 			<div className="recommendations-text">
-				<h1>Género</h1>
+				<h1>Recomendaciones</h1>
 			</div>
 			<div>{recommendations.map(recommendationsToComponent)}</div>
 		</div>
