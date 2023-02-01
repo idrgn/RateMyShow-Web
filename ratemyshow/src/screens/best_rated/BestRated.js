a69e0a82e64fd2b46d34325054d1cfcfca6dae52
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-01 20:50:48 +0100

    Añadidos datos a BestRated

--- ratemyshow/src/screens/best_rated/BestRated.js
+++ ratemyshow/src/screens/best_rated/BestRated.js
@@ -1,5 +1,27 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import TitleList from "../../components/title_list/TitleList";
+
 const BestRated = () => {
-	return <div></div>;
+	// Creamos estado para almacenar la respuesta
+	const [searchResults, setSearchResults] = useState({ best: [] });
+
+	// Obtención de datos
+	useEffect(() => {
+		axios.get(`http://api.ratemyshow.lekiam.net/best`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
+			setSearchResults(response.data);
+			console.log(JSON.stringify(response.data));
+		});
+	}, []);
+
+	return (
+		<div>
+			<div className="search-title">Mejor Valoradas</div>
+			<div className="search-result">
+				<TitleList titles={searchResults.best}></TitleList>
+			</div>
+		</div>
+	);
 };
 
 export default BestRated;
