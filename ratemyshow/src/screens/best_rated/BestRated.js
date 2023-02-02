164ffe6d1a75d941985e170ecae92ef4af392285
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-02 09:26:29 +0100

    Añadida animación de carga a BestRated

--- ratemyshow/src/screens/best_rated/BestRated.js
+++ ratemyshow/src/screens/best_rated/BestRated.js
@@ -1,18 +1,23 @@
 import { Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loading from "../../components/loading/Loading";
 import TitleList from "../../components/title_list/TitleList";
 import "./BestRated.css";
 
 const BestRated = () => {
 	// Creamos estado para almacenar la respuesta
 	const [bestRated, setBestRated] = useState({ result: [] });
+	const [isLoading, setIsLoading] = useState(true);
 
 	// Obtención de datos
 	useEffect(() => {
-		axios.get(`http://api.ratemyshow.lekiam.net/best`, { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-			setBestRated(response.data);
-		});
+		axios
+			.get(`http://api.ratemyshow.lekiam.net/best`, { headers: { SessionToken: localStorage.getItem("sessionToken") } })
+			.then((response) => {
+				setBestRated(response.data);
+			})
+			.finally(setIsLoading(false));
 	}, []);
 
 	return (
@@ -20,9 +25,7 @@ const BestRated = () => {
 			<div className="bestrated-title">
 				<Typography variant="h2"> Títulos Mejor Valorados</Typography>
 			</div>
-			<div className="bestrated-result">
-				<TitleList titles={bestRated.result}></TitleList>
-			</div>
+			<div className="bestrated-result">{isLoading ? <Loading></Loading> : <TitleList titles={bestRated.result}></TitleList>}</div>
 		</div>
 	);
 };
