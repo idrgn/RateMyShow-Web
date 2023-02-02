f01b05f702a573f6d507fe8120a036f10410c42c
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-02 09:41:30 +0100

    Añadida paginación a BestRated

--- ratemyshow/src/screens/best_rated/BestRated.js
+++ ratemyshow/src/screens/best_rated/BestRated.js
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Pagination, Typography } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Loading from "../../components/loading/Loading";
@@ -9,6 +9,7 @@ const BestRated = () => {
 	// Creamos estado para almacenar la respuesta
 	const [bestRated, setBestRated] = useState({ result: [] });
 	const [isLoading, setIsLoading] = useState(true);
+	const [page, setPage] = useState(0);
 
 	// Obtención de datos
 	useEffect(() => {
@@ -20,12 +21,21 @@ const BestRated = () => {
 			.finally(setIsLoading(false));
 	}, []);
 
+	const onPageChange = (event, value) => {
+		if (page !== value - 1) {
+			setPage(value - 1);
+		}
+	};
+
 	return (
 		<div className="bestrated-container">
 			<div className="bestrated-title">
 				<Typography variant="h2"> Títulos Mejor Valorados</Typography>
 			</div>
 			<div className="bestrated-result">{isLoading ? <Loading></Loading> : <TitleList titles={bestRated.result}></TitleList>}</div>
+			<div className="bestrated-pagination" color="primary" size="large">
+				<Pagination count={bestRated.pages} onChange={onPageChange} />
+			</div>
 		</div>
 	);
 };
