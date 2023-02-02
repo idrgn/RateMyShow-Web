b4b9d9f2f146d5b3afe0ea1388897d43a6317792
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-02 22:07:01 +0100

    Petición de logout dentro de useEffect

--- ratemyshow/src/screens/logout/Logout.js
+++ ratemyshow/src/screens/logout/Logout.js
@@ -1,24 +1,27 @@
 import axios from "axios";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Loading from "../../components/loading/Loading";
 
 const Logout = () => {
 	const navigate = useNavigate();
 
-	// Se envía la petición de cerrar sesión
-	axios.delete("http://api.ratemyshow.lekiam.net/sessions", { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
-		// Eliminar session token almacenado
-		localStorage.removeItem("sessionToken");
+	useEffect(() => {
+		// Se envía la petición de cerrar sesión
+		axios.delete("http://api.ratemyshow.lekiam.net/sessions", { headers: { SessionToken: localStorage.getItem("sessionToken") } }).then((response) => {
+			// Eliminar session token almacenado
+			localStorage.removeItem("sessionToken");
 
-		// Eliminar datos de usuario almacenados
-		localStorage.removeItem("username");
-		localStorage.removeItem("name");
-		localStorage.removeItem("surname");
-		localStorage.removeItem("avatarId");
+			// Eliminar datos de usuario almacenados
+			localStorage.removeItem("username");
+			localStorage.removeItem("name");
+			localStorage.removeItem("surname");
+			localStorage.removeItem("avatarId");
 
-		// Redirección a login
-		navigate("/login");
-	});
+			// Redirección a login
+			navigate("/login");
+		});
+	}, [navigate]);
 
 	return (
 		<div>
