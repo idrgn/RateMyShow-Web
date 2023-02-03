ba24519d6f63ffc2f26d1ca4e68b4be09348b092
idrgn <37042855+idrgn@users.noreply.github.com>
2023-02-03 10:03:31 +0100

    Añadido código para buscar usuarios

--- ratemyshow/src/screens/users/Users.js
+++ ratemyshow/src/screens/users/Users.js
@@ -1,3 +1,45 @@
-const Users = () => {};
+import { Pagination } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import UserListItem from "../../components/user_list_item/UserListItem";
+
+const Users = (props) => {
+	// Creamos estado para almacenar la lista de usuarios
+	const [response, setResponse] = useState({ users: [] });
+	const [search, setSearch] = useState[""];
+	const [page, setPage] = useState(0);
+
+	// Pedimos los datos a la API
+	useEffect(() => {
+		axios.get(`http://api.ratemyshow.lekiam.net/users?query=${search}&page=${page}`).then((response) => {
+			setResponse(response.data);
+		});
+	}, [page, search]);
+
+	// Actualizar página
+	const onPageChange = (event, value) => {
+		// Solo se actualiza si el valor cambia
+		if (page !== value - 1) {
+			setPage(value - 1);
+		}
+	};
+
+	// Función para convertir lista de usuarios a componente
+	const userListToComponent = (u) => {
+		return <UserListItem user={u} />;
+	};
+
+	return (
+		<div>
+			<div>
+				<h1>Usuarios</h1>
+			</div>
+			<div>{response.users.map(userListToComponent)}</div>
+			<div>
+				<Pagination count={response.pages} onChange={onPageChange} />
+			</div>
+		</div>
+	);
+};
 
 export default Users;
